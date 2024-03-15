848bf94b00d2f6405eda0ae78bdca3b770ce3325
CedricDLMS <delmascede@gmail.com>
2024-03-15 14:27:50 +0100

    minor changes

---
+++ snakeTest1/Classe/Snake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeTest1.Classe
+{
+    internal class snake
+    {
+    }
+}
--- snakeTest1/obj/Debug/net8.0/snakeTest1.AssemblyInfo.cs
+++ snakeTest1/obj/Debug/net8.0/snakeTest1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("snakeTest1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c9c408afcf4fc13e584b5ef8247166746f10a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("snakeTest1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("snakeTest1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
