6514b2a5f7114a7e75000202f9bfdd6f68cf34c8
CedricDLMS <delmascede@gmail.com>
2024-03-15 14:29:16 +0100

    first changes

--- snakeTest1/Classe/Display.cs
+++ snakeTest1/Classe/Display.cs
@@ -8,5 +8,14 @@ namespace snakeTest1.Classe
 {
     internal class Display
     {
+        public int maxWidth { get; set; }
+        public int maxHeight { get; set; }
+        public int[,] coordinateDisplay { get; private set ; }
+
+        public void DisplayInit() { 
+            this.coordinateDisplay = new int[maxWidth, maxHeight];
+        }
+        
+        
     }
 }
--- snakeTest1/Classe/Snake.cs
+++ snakeTest1/Classe/Snake.cs
@@ -8,5 +8,9 @@ namespace snakeTest1.Classe
 {
     internal class snake
     {
+        public int xPos {  get; set; }
+        public int yPos { get; set; }
+        public int snakeLenght {  get; set; }
+
     }
 }
